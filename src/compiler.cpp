a3897cf2792ebee7452e78e2fb215ab722132dfc
horsada <ah2719@ic.ac.uk>
2021-03-26 00:40:27 +0000

    fixed main argv

--- src/compiler.cpp
+++ src/compiler.cpp
@@ -24,21 +24,21 @@ int main(){
 }
 */
 int main(int argc, char* argv[]){
-    if(argc < 4){
+    if(argc < 5){
         fprintf(stderr, "usage : compiler sourceCode\n");
         exit(1);
     }
     
-    std::ofstream out(argv[3]);
-    std::string dest = argv[3];
+    std::ofstream out(argv[4]);
+    std::string dest = argv[4];
     std::streambuf *coutbuf = std::cout.rdbuf(); //save old buf
     std::cout.rdbuf(out.rdbuf()); //redirect std::cout to out.txt!
 
-    std::ifstream in(argv[1]);
+    std::ifstream in(argv[2]);
     std::streambuf *cinbuf = std::cin.rdbuf(); //save old buf
     std::cin.rdbuf(in.rdbuf()); //redirect std::cout to out.txt!
 
-    std::string src = argv[1];
+    std::string src = argv[2];
     
     ExpressionPtr ast = parseAST(src);
     std::cout << "*---------------------PARSED CONTENT-----------------*" << std::endl;
